2a8e605a01d3baf3ef769f008f098eb6cccbcaaa
sramosbernal22 <sramosbernal22@gmail.com>
2024-05-25 16:16:54 -0500

    Implemented getComputerChoice which returns a rock paper scissor randomly.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+
+let getComputerChoice = () => {
+    let pick = Math.floor(Math.random()*3);
+    //console.log(pick)//Check if my number is coming out right
+    if(pick == 0)
+    {
+        return 'rock';   
+    }
+    else if(pick == 1)
+    {
+        return 'paper';
+    }
+    else if(pick == 2)
+    {
+        return 'scissors';
+    }
+
+};
+
+console.log(getComputerChoice())
